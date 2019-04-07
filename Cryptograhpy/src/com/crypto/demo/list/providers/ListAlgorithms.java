ec78ef6f65cc453e771b3cd089498b3b526eec69
kablu mandal <kablumndl546@gmail.com>
2019-04-07 06:47:21 +0530

    Add files via upload

--- Cryptograhpy/src/com/crypto/demo/list/providers/ListAlgorithms.java
+++ Cryptograhpy/src/com/crypto/demo/list/providers/ListAlgorithms.java
@@ -2,6 +2,9 @@ package com.crypto.demo.list.providers;
 
 import java.security.Provider;
 import java.security.Security;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  * 
@@ -10,7 +13,75 @@ import java.security.Security;
  */
 public class ListAlgorithms {
 
+	 public static void printSet(
+		        String setName,
+		        Set	   algorithms)
+		    {
+		        System.out.println(setName + ":");
+		        
+		        if (algorithms.isEmpty())
+		        {
+		            System.out.println("            None available.");
+		        }
+		        else
+		        {
+		            Iterator	it = algorithms.iterator();
+		            
+		            while (it.hasNext())
+		            {
+		                String	name = (String)it.next();
+		                
+		                System.out.println("            " + name);
+		            }
+		        }
+		    }
+	 
 	public static void main(String[] args) {
 		Provider[] providers = Security.getProviders();
+		Set ciphers  = new HashSet();
+		Set keyAggreements  = new HashSet();
+		Set macs = new HashSet();
+		Set messagedigest  = new HashSet();
+		Set signatures  = new HashSet();
+		
+		
+		for (int i = 0; i < providers.length; i++) {
+			
+			Iterator it = providers[i].keySet().iterator();
+			
+			while (it.hasNext()) {
+				String entry = (String) it.next();
+				
+				System.out.println("entry:" + entry);
+				
+				if(entry.startsWith("Alg.Alias.")){
+					entry = entry.substring("Alg.Alias.".length());
+				}
+				
+				if(entry.startsWith("Cipher.")){
+					ciphers.add(entry.substring("Cipher.".length()));
+				}
+				else if(entry.startsWith("KeyAgreement.")){
+					keyAggreements.add(entry.substring("KeyAgreement.".length()));
+				}else if (entry.startsWith("Mac."))
+                {
+                    macs.add(entry.substring("Mac.".length()));
+                }
+                else if (entry.startsWith("MessageDigest."))
+                {
+                	messagedigest.add(entry.substring("MessageDigest.".length()));
+                }
+                else if (entry.startsWith("Signature."))
+                {
+                    signatures.add(entry.substring("Signature.".length()));
+                }
+			}
+			
+		}
+		 printSet("Ciphers", ciphers);
+	     printSet("KeyAgreeents", keyAggreements);
+	     printSet("Macs", macs);
+	     printSet("MessageDigests", messagedigest);
+	     printSet("Signatures", signatures);
 	}
 }
